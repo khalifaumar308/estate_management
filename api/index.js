68e4ab93232cca29804dde71c8ca6cc4076cd935
Umar Muhammad Aminu <khalifaumar308@gmail.com>
2023-10-11 12:52:11 +0100

    Connect Mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() =>{
+    console.log('Connected succc')
+  })
+  .catch((err) => {
+    console.log(err)
+  });
 
 const app = express();
 
